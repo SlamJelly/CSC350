89c17e2851ff002d8c1809cf7737cfc66c6d2d3a
geeoo3 <oderg@my.lr.edu>
2016-11-30 06:46:06 -0500

    Add files via upload

--- Form1.cs
+++ Form1.cs
@@ -8,36 +8,16 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace car_rental_practice
+namespace RentalAgreement
 {
-    public partial class lblAC : Form
+    public partial class Form1 : Form
     {
-        public lblAC()
+        public Form1()
         {
             InitializeComponent();
         }
 
         private void Form1_Load(object sender, EventArgs e)
-        {
-
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void txtCostOfRental_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void lblVehicleType_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label3_Click(object sender, EventArgs e)
         {
 
         }
